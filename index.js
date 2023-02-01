b4166aca9cd663184641628198ef87301340a9fc
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2023-02-01 12:37:22 +0100

    working on extras javascript

---
+++ index.js
@@ -0,0 +1,20 @@
+//Extra Exercises
+/* Ex 1
+ // EXTRA Write a JavaScript functionality to remove the “twitter” link under the “Elsewhere” section in the aside element. It should happen when the page loads, automatically.
+ */
+let twl = 
+ 
+
+
+
+
+ /*Ex 2
+ EXTRA Write a JavaScript functionality that will remove the corresponding parent card from the DOM, upon clicking on their “Continue reading” link
+ */
+
+
+
+
+ /*Ex 3
+  EXTRA Write a JavaScript functionality that will create an alert with the name of the author every time the user hovers with the mouse on an author name
+ */
